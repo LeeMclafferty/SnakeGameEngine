3c07957ff95e01f7bb9bacef29d03a82409d10da
Lee McLafferty <lee.mclafferty@gmail.com>
2025-01-14 20:41:06 -0800

    Added basic event handling and FPS control

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Snake Game Engine");
+	
+	sf::RectangleShape shape(sf::Vector2(20.f, 20.f));
 	shape.setFillColor(sf::Color::Green);
 
+	sf::Clock clock;
+	window.setFramerateLimit(60);
 	while (window.isOpen())
 	{
+		// Compute the frame rate
+		float current_time = clock.restart().asSeconds();
+		float fps = 1.0f / (current_time);
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Scan::X) {
+					window.close();
+				}
+				else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+					shape.setPosition(shape.getPosition() + sf::Vector2(0.f, 5.f));
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+					shape.setPosition(shape.getPosition() + sf::Vector2(0.f, -5.f));
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+					shape.setPosition(shape.getPosition() + sf::Vector2(5.f, 0.f));
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+					shape.setPosition(shape.getPosition() + sf::Vector2(-5.f, 0.f));
+				}
+			}
 		}
+		
+
+		window.setTitle("Snake Game Engine - FPS: " + std::to_string(static_cast<int>(fps)));
 
 		window.clear();
 		window.draw(shape);
